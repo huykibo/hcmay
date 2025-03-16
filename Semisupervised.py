45596521228be78195a42d7b4488fc2d470de4c4
huykibo <hkb.tkb@gmail.com>
2025-03-17 01:55:14 +0700

    Initial commit: Clean setup for Streamlit MLflow app

--- Semisupervised.py
+++ Semisupervised.py
@@ -59,7 +59,7 @@ def get_optimal_params(num_samples):
 
 def run_mnist_pseudo_labeling_app():
     # Thiết lập MLflow
-    mlflow_tracking_uri = "https://dagshub.com/huykibo/MNIST_Pseudo_Labeling.mlflow"
+    mlflow_tracking_uri = "https://dagshub.com/huykibo/streamlit_mlflow.mlflow"
     try:
         os.environ["MLFLOW_TRACKING_USERNAME"] = st.secrets["mlflow"]["MLFLOW_TRACKING_USERNAME"]
         os.environ["MLFLOW_TRACKING_PASSWORD"] = st.secrets["mlflow"]["MLFLOW_TRACKING_PASSWORD"]
